aa2bdfb6c12a02ab22a20b0d5c88d78fc04804d0
benikib <benikasu7@gmail.com>
2025-01-24 14:44:08 +0100

    envoi des information vers api

--- lib/models/modelPharmacie.dart
+++ lib/models/modelPharmacie.dart
@@ -1,38 +1,62 @@
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:opharma/utils/Endpoint.dart';
 import '../session/Session.dart';
 import 'baseDeDonnee/BaseDeDonnee.dart';
 
-
-
-class ModelPharmacie  {
-  late   int ? id_pharmacie;
+class ModelPharmacie {
+  late int? id_pharmacie;
   String nom_pharmacie;
-
   String adresse_physique;
-
   String longitude;
   String latutude;
 
-  String nomTable="pharmacie";
-  static BaseDeDonnee base=new  BaseDeDonnee();
+  String nomTable = "pharmacie";
+  static BaseDeDonnee base = new BaseDeDonnee();
 
-  ModelPharmacie(this.nom_pharmacie,this.adresse_physique,this.longitude,this.latutude);
+  ModelPharmacie(this.nom_pharmacie, this.adresse_physique, this.longitude, this.latutude);
 
-  ajouter() async{
-    int id_phar=await ModelPharmacie.base.ajoutDonnees(this.nomTable,{"nom_pharmacie":this.nom_pharmacie,
-      "adresse_physique":this.adresse_physique,
-      "longitude":this.latutude,
-      "latitude":this.longitude,
-
+  ajouter() async {
+    // Ajout dans la base locale
+    int id_phar = await ModelPharmacie.base.ajoutDonnees(this.nomTable, {
+      "nom_pharmacie": this.nom_pharmacie,
+      "adresse_physique": this.adresse_physique,
+      "longitude": this.longitude,
+      "latitude": this.latutude,
     });
-    return  id_phar;
 
+    // Prépare les données pour l'API
+    Map<String, dynamic> data = {
+      "nom_pharmacie": this.nom_pharmacie,
+      "adresse_physique": this.adresse_physique,
+      "longitude": this.longitude,
+      "latitude": this.latutude,
+    };
+
+    // Envoie les données à l'API
+    Uri url = Uri.parse(Endpoint.baseUrlEnregisterpharmacie);
+    try {
+      var response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 201) {
+        print("Données envoyées avec succès : ${response.body}");
+      } else {
+        print("Erreur lors de l'envoi : ${response.statusCode}, ${response.body}");
+      }
+    } catch (e) {
+      print("Erreur réseau : $e");
+    }
+
+    return id_phar;
   }
 
   static affId(int id) {
-    String requette="select * from pharmacie where id_pharmacie=$id";
+    String requette = "select * from pharmacie where id_pharmacie=$id";
     return ModelPharmacie.base.reccuperationDonnees(requette);
   }
-
-
 }
+
---
+++ lib/utils/Endpoint.dart
@@ -0,0 +1,10 @@
+class Endpoint {
+
+  static const String baseUrl = "http://http://127.0.0.1:8000/api/";
+  static const String baseUrlLogin = baseUrl + "auth/login";
+  static const String baseUrlEnregisterpharmacie = baseUrl + "auth/pharmacies";
+  static const String baseUrlCategories =  baseUrl+"categories";
+  static const String baseUrlNotification =  baseUrl+"my/notifications";
+  static const String baseUrlChamps =  baseUrl+"my/cartography-assets";
+
+}
