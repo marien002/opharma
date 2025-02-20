590eaccb57955a804ce0efc0f4755ad91594b015
marien08 <manimamarien08@gmail.com>
2025-02-20 12:21:26 -0800

    correction des fautes d'orthographe

--- lib/view/auth/localisationPhamacie.dart
+++ lib/view/auth/localisationPhamacie.dart
@@ -64,7 +64,6 @@ class _SignalementPageState extends State<SignalementPage> {
       headers: {"Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
       },
-
     );
 
     if (response.statusCode == 200) {
@@ -145,7 +144,6 @@ class _SignalementPageState extends State<SignalementPage> {
   }
 
   void _addMarkers(List pharmacies) {
-
     Set<Marker> newMarkers = pharmacies.map((pharmacy) {
       return Marker(
         width: 80,
--- lib/view/pharmacie/pageEnregistrement.dart
+++ lib/view/pharmacie/pageEnregistrement.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import '../../controlers/controler_pharmacie.dart';
 import '../../controlers/controllerAuth.dart';
 import 'package:opharma/elper/navigation.dart';
-import '../auth/pageAuthentificationPharma.dart';
 import '../componentGenerale/ButtonCostom.dart';
 import '../componentGenerale/Combobox.dart';
 import '../componentGenerale/InputCostom.dart';
 import '../componentGenerale/entete.dart';
 import '../leyouts/base.dart';
-import 'package:geolocator/geolocator.dart';
+
 
 class pageEnregistrement extends StatefulWidget {
   static String nom_pharmacie="";
@@ -20,15 +18,10 @@ class pageEnregistrement extends StatefulWidget {
   static String longitude="";
   static String latitude="";
 
-
   @override
   State<pageEnregistrement> createState() => pageEnregistrementState();
 }
 class pageEnregistrementState extends State<pageEnregistrement> {
-  Position? _currentPosition;
-  bool etat = false;
-  bool pswdVisible = false;
-  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     var h=MediaQuery.of(context).size;
@@ -38,38 +31,26 @@ class pageEnregistrementState extends State<pageEnregistrement> {
     var colorInput=Color.fromRGBO(230, 230, 230,1);
 
     ButtonCostom localisation= ButtonCostom("Localisation via la map",colorButton,(){
-    _getCurrentPosition();
+
     },rad: 9);
 
     InputCostom nomPharma=InputCostom(Name:"nomPharma",lar:longInp,long:largInp,
         value: "Nom du pharmacie",
-        couleur:colorInput,
-        icon: Icon(Icons.local_pharmacy_outlined)
+        couleur:colorInput
     );
     InputCostom adresse=InputCostom(Name:"Adresse_physique",lar:longInp,long:largInp,
         value: "Adresse physique",
-        couleur:colorInput, 
-      icon: Icon(Icons.home_filled)
-        
+        couleur:colorInput
     );
 
 
     InputCostom login=InputCostom(Name:"login",lar:longInp,long:largInp,
-        value: "Téléphone ou Email",
-        couleur:colorInput,
-        icon: Icon(Icons.login)
+        value: "login",
+        couleur:colorInput
     );
     InputCostom motDePasse=InputCostom(Name:"motDePasse",lar:longInp,long:largInp,
-        value: "Mot de passe",
-        couleur:colorInput,
-        icon: IconButton(onPressed: (){
-        setState(() {
-          pswdVisible = !pswdVisible;
-        });
-      }, icon: pswdVisible ?Icon( Icons.visibility ,color: Color.fromRGBO(50, 190, 166, 1)) :Icon( Icons.visibility_off,color: Color.fromRGBO(50, 190, 166, 1))),
-
-      estcache: pswdVisible,
-
+        value: "mot de passe",
+        couleur:colorInput
     );
     InputCostom motDePasseConfirmation=InputCostom(Name:"motDePasseConfirmation",lar:longInp,long:largInp,
         value: "confirmer votre mot de passe",
@@ -86,114 +67,27 @@ class pageEnregistrementState extends State<pageEnregistrement> {
             logo: null
         ).Demarrer(),
 
-          bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10),
-        child: isLoading ?SizedBox.square(): _authentification(),
-      ),
-
-       body: Stack( children: [
-
-         Base(
+        body:Base(
           content: Column(
             children: [
               blockEnregistrement(
                 "Créer un compte pharmacie ",
                 [
-                  SizedBox(height: 20,),
                   nomPharma.lancer(),
-                  SizedBox(height: 20,),
-                 login.lancer(),
-                  SizedBox(height: 20,),
                   adresse.lancer(),
-                  SizedBox(height: 20,),
+                 login.lancer(),
                  motDePasse.lancer(),
-                  SizedBox(height: 20,),
-                  _checkBox(),
-                  SizedBox(height: 20,),
+                  localisation.lancer(),
 
                   ButtonCostom("Créer le compte",colorButton,(){
-                    setState(() {
-                      isLoading = true; // Fin du processus de chargement
-                    });
-
-                    if(nomPharma.ValueAf() == null || nomPharma.ValueAf().isEmpty ||
-                        adresse.ValueAf() == null || adresse.ValueAf().isEmpty ||
-                        login.ValueAf() == null || login.ValueAf().isEmpty ||
-                        motDePasse.ValueAf() == null || motDePasse.ValueAf().isEmpty) {
-                      Fluttertoast.showToast(
-                        msg: "Veuillez remplir tous les champs obligatoires.",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.TOP,
-                        backgroundColor: Colors.redAccent,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                      setState(() {
-                        isLoading = false; // Fin du processus de chargement
-                      });
-
-                      return;
-                    }
-                    // Validation du login (email ou numéro de téléphone)
-                    final emailRegex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-                    final phoneRegex = RegExp(r"^\+?[0-9]{10,15}$"); // Accepte un numéro international ou national
-
-                    if (!emailRegex.hasMatch(login.ValueAf()) && !phoneRegex.hasMatch(login.ValueAf())) {
-                      Fluttertoast.showToast(
-                        msg: "Veuillez entrer un email ou un numéro de téléphone valide.",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.TOP,
-                        backgroundColor: Colors.orangeAccent,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                      setState(() {
-                        isLoading = false; // Fin du processus de chargement
-                      });
-
-                      return;
-                    }
-                    // Validation du mot de passe
-                    final passwordRegex = RegExp(r'^(?=.*?[!@#\$&*~]).{8,}$'); // Au moins 8 caractères et un caractère spécial
-                    if (!passwordRegex.hasMatch(motDePasse.ValueAf())) {
-                      Fluttertoast.showToast(
-                        msg: "Le mot de passe doit contenir au moins 8 caractères et un caractère spécial.",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.TOP,
-                        backgroundColor: Colors.orangeAccent,
-                        textColor: Colors.white,
-                        fontSize: 16.0,
-                      );
-                      setState(() {
-                        isLoading = false; // Fin du processus de chargement
-                      });
-
-                      return;
-                    }
-
-                    else{
-
-                      print("${_currentPosition?.latitude}   ${_currentPosition?.longitude}");
-
                     Controler_pharmacie(context).Enregistrer(
                        nom_pharmacie:  nomPharma.ValueAf(),
                         adresse_physique: adresse.ValueAf(),
                         login: login.ValueAf(),
                        mot_de_passe: motDePasse.ValueAf(),
-                        latitude: "${_currentPosition?.latitude}",
-                        longitude: "${_currentPosition?.longitude}",
-                ).then((_) {
-                    setState(() {
-                      isLoading = false; // Arrêter le spinner après l'enregistrement
-                    });
-                    }).catchError((error) {
-                    setState(() {
-                         isLoading = false; // En cas d'erreur aussi
-                    });
-                    print("errorrrrr d enregistrement : $error");
-                    Fluttertoast.showToast(msg: "une  erreur ce produit lors d'envoi");
-                    });
-                    };
+                        latitude: "22",
+                        longitude: "22",
+                );
                   },rad: 9).lancer()
 
 
@@ -205,94 +99,8 @@ class pageEnregistrementState extends State<pageEnregistrement> {
           ) ,
           child: []
         ).lancer(h.height-270,h.width-25),
-    if (isLoading)
-    Container(
-    color: Colors.black.withOpacity(0.32), // Fond noir semi-transparent
-    child: Center(
-    child: CircularProgressIndicator(color:Color.fromRGBO(50, 190, 166, 1) ,strokeWidth: 4,),
-    ),)],)
     );
 
-  }
-  _authentification() {
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(" Vous avez deja un "),
-          TextButton(onPressed: () {
-
-            navigation(context,pageAuthentificationPharma());
-          },
-              child: Text("Compte",
-               // style: GoogleFonts.montserrat(color: MyColor.c1),
-              ))
-        ]);
-  }
-  Widget _checkBox() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Checkbox(
-          value: etat,
-          onChanged: (value) {
-            setState(() {
-              etat = value ?? false;
-              if (etat) {
-                _getCurrentPosition();
-              }
-            });
-
-
-
-          },
-          activeColor: const Color.fromRGBO(50, 190, 166, 1),
-        ),
-        Expanded(
-          child: Text(
-            "J’ai lu et j’accepte les Termes et Conditions dont "
-                "les conditions générales d’utilisation et la Politique de Confidentialité",
-            maxLines: 3,
-          ),
-        )
-      ],
-    );
-  }
-
-  // Méthode pour obtenir la position actuelle
-  Future<void> _getCurrentPosition() async {
-    try {
-      // Vérifie si le service de localisation est activé
-      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      if (!serviceEnabled) {
-        throw Exception('Le service de localisation est désactivé.');
-      }
-
-      // Vérifie et demande les permissions de localisation
-      LocationPermission permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.denied) {
-        permission = await Geolocator.requestPermission();
-        if (permission == LocationPermission.denied) {
-          throw Exception('La permission de localisation est refusée.');
-        }
-      }
-
-      if (permission == LocationPermission.deniedForever) {
-        throw Exception(
-            'La permission de localisation est refusée en permanence.');
-      }
-
-      // Obtient la position actuelle
-      final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
-
-      setState(() {
-        _currentPosition = position;
-      });
-    } catch (e) {
-      // Affiche une erreur si quelque chose échoue
-      print('Erreur: $e');
-    }
   }
 
 }
@@ -320,8 +128,8 @@ Widget  blockEnregistrement(String title,List<Widget> element,{tailleT=60}){
     child:Padding(
       padding: EdgeInsets.only(left: 10,right: 10,bottom: 10),
       child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: element,
       ),
     ) ,
